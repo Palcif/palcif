f18175cde5b595158c43370f6ecb944390a6c000
Amjad Shakhshir <amjadshakhshir73@gmail.com>
2026-08-03 23:44:20 +0300

    feat: redesign home upcoming events as an image grid, cache GraphQL GET requests

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,4 +2,6 @@ import { GraphQLClient } from 'graphql-request'
 
 const endpoint = import.meta.env['VITE_WP_GRAPHQL_URL'] as string
 
-export const gqlClient = new GraphQLClient(endpoint)
+// GET (not POST) so Cloudflare and WPGraphQL Smart Cache's network cache can
+// cache and serve responses instead of every request hitting WordPress.
+export const gqlClient = new GraphQLClient(endpoint, { method: 'GET' })
